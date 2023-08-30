0303d805013f05a30a6fe608c6d361d5cfc6c7ee
danielnachumdev <danielnachumdev@gmail.com>
2023-08-31 01:13:34 +0300

    initial commit

---
+++ generate_md.py
@@ -0,0 +1,28 @@
+from objects import *  # pylint: disable=wildcard-import,unused-wildcard-import
+
+LANGUAGES_AND_TOOLS = sorted([
+    "python", "c", "cplusplus", "csharp", "java",
+    "javascript", "css3", "html5", "postgresql", "make",
+    "cmake", "git", "linux", "docker", "vim",
+    "jetbrains", "markdown", "mongodb", "sql", "npm",
+    "numpy", "pandas", "pytest", "react", "visualstudio",
+    "anaconda", "blender", "bootstrap", "canva", "express",
+    "gcc", "github"
+])
+
+README: list[Markdownable] = [
+    Comment("markdownlint-disable MD033 MD041"),
+    Heading("Daniel Nachum"),
+    Heading("Languages & Tools", 2),
+    * [IconSvg(name) for name in LANGUAGES_AND_TOOLS],
+]
+
+
+def main() -> None:
+    with open("./README.md", "w", encoding="utf8") as f:
+        for obj in README:
+            f.write(obj.to_markdown())
+
+
+if __name__ == "__main__":
+    main()
---
+++ objects.py
@@ -0,0 +1,65 @@
+from typing import Optional
+from abc import ABC, abstractmethod
+
+
+class Markdownable(ABC):
+    @abstractmethod
+    def to_markdown(self) -> str: ...
+
+
+class Comment(Markdownable):
+    def __init__(self, comment: str) -> None:
+        self.comment = comment
+
+    def to_markdown(self) -> str:
+        return f"<!-- {self.comment}-->\n"
+
+
+class Heading(Markdownable):
+    def __init__(self, text: str, size: int = 1) -> None:
+        self.size = size
+        self.text = text
+
+    def to_markdown(self) -> str:
+        return f"\n{'#'*self.size} {self.text}\n"
+
+
+class Image(Markdownable):
+    STRING = "<img alt={alt} width={width} height={height} style={style} src=\"{src}\" />\n"
+
+    def __init__(self, src: str, width: Optional[int], height: Optional[int], alt: str, style: Optional[dict] = None) -> None:
+        self.src = src
+        self.width = width
+        self.height = height
+        self.alt = alt
+        self.style = style
+
+    def to_markdown(self) -> str:
+        return Image.STRING.format(
+            alt=self.alt,
+            width=self.width,
+            height=self.height,
+            style=self.style if self.style else '""',
+            src=self.src
+        )
+
+
+class Break(Markdownable):
+    def to_markdown(self) -> str:
+        return "<br/>\n"
+
+
+class IconSvg(Image):
+    def __init__(self, name: str) -> None:
+        src = f"https://raw.githubusercontent.com/devicons/devicon/master/icons/{name}/{name}-original.svg"
+        super().__init__(src, 40, 40, name, None)
+
+
+__all__ = [
+    "Markdownable",
+    "Comment",
+    "Heading",
+    "Image",
+    "Break",
+    "IconSvg"
+]
